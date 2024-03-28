783b036fb2054120004ba3d741d2cafdfdcc770d
Pieter Vander Vennet <pietervdvn@posteo.net>
2024-03-28 16:40:45 +0100

    Add NoGeo testcase

--- test.ts
+++ test.ts
@@ -56,25 +56,37 @@ const queryWithoutShape = `
                 ] }.
        }`
 
-let sources = ["NoShape.jsonld", "Shape.jsonld"]
 
+const queryWithoutCoords = `
+        ?parking a <http://schema.mobivoc.org/BicycleParkingStation>.
+        GRAPH ?g {
+            VALUES ?type { vpt:BicycleLocker vpt:BicycleStand vpt:ResidentBicycleParking vpt:PublicBicycleParking }.
+            ?section a ?type.
+            OPTIONAL { ?section schema:geo ?geo }.
+       }`
+
+let sources = ["NoShape.jsonld", "Shape.jsonld", "NoGeo.jsonld"]
+
+let queries: Record<string, string>  = {
+    withShape: queryWithShape,
+    withoutShape: queryWithoutShape,
+    noCoords: queryWithoutCoords
+}
 async function main() {
-    const comunica =new QueryEngine()
+    const comunica = new QueryEngine()
     for (const source of sources) {
-        for (let i = 0; i < [queryWithoutShape, queryWithShape].length; i++){
-            const query = [queryWithoutShape, queryWithShape][i];
+        for (const queryName in queries) {
+            const query = queries[queryName]
 
             const q = buildQuery([query], prefixes)
             const url = "http://127.0.0.1:8080/"+source
-            console.log("source is", url)
+          //  console.log("source is", url, q)
             const bindingsStream = await comunica.queryBindings(
-                q, {sources: [url]}
+                q, {sources: [url], lenient: true},
             )
             const bindings = await bindingsStream.toArray()
-            console.log("Got "+bindings.length+" bindings for "+url+" with query "+(i == 0 ? 'no shape' : "with shape")+":")
-            bindings.forEach(b => {
-                console.log("    "+b.toString())
-            })
+            console.log("Got "+bindings.length+" bindings for "+url+" with query "+queryName+":")
+            // console.log(bindings.map(b => b.toString()).join(","))
         }
     }
 
